daa6a003f54044c1722da84bb82e5a731b64c81a
Uzair Ashraf <uzinatorcl@live.com>
2020-09-14 15:16:13 -0700

    starting line working, need a preview for the user though

--- main.js
+++ main.js
@@ -5,8 +5,18 @@ const state = {
   mouseCoords: {
     x: 0,
     y: 0
+  },
+  linePoint: {
+    startingX: null,
+    startingY: null,
+    endingX: null,
+    endingY: null
   }
 }
+
+// Canvas Event Listener History
+let currentEvents = []
+
 
 // DOM Elements
 const $toolBarContainer = document.querySelector('.tool-bar')
@@ -14,16 +24,13 @@ const $colorPalette = document.querySelector('.color-palette')
 const $currentColor = document.querySelector('.current-color')
 const $canvas = document.querySelector('canvas')
 
-// Events
-$colorPalette.addEventListener('click', switchColor)
-$toolBarContainer.addEventListener('click', switchTool)
-$canvas.addEventListener('mousemove', pencil)
-$canvas.addEventListener('mousedown', setMouseCoords)
-$canvas.addEventListener('mouseenter', setMouseCoords)
-
 // initialization
 const ctx = $canvas.getContext('2d')
 init()
+
+// Events
+$colorPalette.addEventListener('click', switchColor)
+$toolBarContainer.addEventListener('click', switchTool)
 
 function init() {
   // setup toolbar images
@@ -50,10 +57,11 @@ function init() {
     state.selectedTool = document.querySelector(`div[data-tool=${state.selectedTool}]`)
   }
   state.selectedTool.classList.add('selected')
+  setCanvasListenersBasedOffTool(state.selectedTool.dataset.tool)
 }
 
 function pencil(e) {
-  if(e.buttons !== 1) return;
+  if (e.buttons !== 1) return;
   ctx.beginPath()
   ctx.strokeStyle = state.selectedColor
   ctx.lineWidth = 3
@@ -61,6 +69,41 @@ function pencil(e) {
   setMouseCoords(e)
   ctx.lineTo(state.mouseCoords.x, state.mouseCoords.y)
   ctx.stroke()
+}
+
+function line(e) {
+  if (e.buttons !== 1) return;
+  ctx.strokeStyle = state.selectedColor
+  ctx.lineWidth = 3
+  const {startingX, startingY, endingX, endingY} = state.linePoint
+  if(startingX === null && startingY === null) {
+    const bounds = e.target.getBoundingClientRect()
+    state.linePoint.startingX = e.clientX - Math.floor(bounds.left)
+    state.linePoint.startingY = e.clientY - Math.floor(bounds.top)
+    ctx.moveTo(state.linePoint.startingX, state.linePoint.startingY)
+  }
+    // } else if(endingX === null && endingY === null) {
+  //   ctx.lineTo(state.mouseCoords.x, state.mouseCoords.y)
+  // } else {
+  //   ctx.lineTo(state.linePoint.endingX, state.linePoint.endingY)
+  // }
+
+  // ctx.stroke()
+}
+
+function lineMouseUp(e) {
+  const bounds = e.target.getBoundingClientRect()
+  ctx.strokeStyle = state.selectedColor
+  ctx.lineWidth = 3
+  state.linePoint.endingX = e.clientX - Math.floor(bounds.left)
+  state.linePoint.endingY = e.clientY - Math.floor(bounds.top)
+  ctx.moveTo(state.linePoint.startingX, state.linePoint.startingY)
+  ctx.lineTo(state.linePoint.endingX, state.linePoint.endingY)
+  ctx.stroke()
+  state.linePoint.startingX = null
+  state.linePoint.startingY = null
+  state.linePoint.endingX = null
+  state.linePoint.endingY = null
 }
 
 function setMouseCoords(e) {
@@ -81,7 +124,37 @@ function switchColor(e) {
 
 function switchTool(e) {
   if (!('tool' in e.target.dataset)) return;
+  removeAllCanvasEventListeners()
   state.selectedTool.classList.remove('selected')
   state.selectedTool = e.target
   state.selectedTool.classList.add('selected')
+  setCanvasListenersBasedOffTool(state.selectedTool.dataset.tool)
+}
+
+function setCanvasListenersBasedOffTool(tool) {
+  switch(tool) {
+    case 'pencil':
+      addCanvasEventListener('mousemove', pencil)
+      addCanvasEventListener('mousedown', setMouseCoords)
+      addCanvasEventListener('mouseenter', setMouseCoords)
+    break;
+    case 'line':
+      addCanvasEventListener('mousedown', line)
+      addCanvasEventListener('mousemove', setMouseCoords)
+      addCanvasEventListener('mouseup', lineMouseUp)
+    break;
+  }
+}
+
+function addCanvasEventListener(type, cb) {
+  currentEvents.push({
+    type,
+    cb
+  })
+  $canvas.addEventListener(type, cb)
+}
+
+function removeAllCanvasEventListeners() {
+  currentEvents.forEach(e => $canvas.removeEventListener(e.type, e.cb))
+  currentEvents = []
 }
